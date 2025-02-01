6206dd35f67e7ed3d1fe750ac1056eb2be68429f
WhiffleFish <tylertronics@gmail.com>
2025-02-01 11:07:17 -0700

    dont need bs4

--- occupancy_scraper.py
+++ occupancy_scraper.py
@@ -1,15 +1,14 @@
 import requests
 import re
-from bs4 import BeautifulSoup
 import datetime
 
 URL = 'https://portal.rockgympro.com/portal/public/415a34a23151c6546419c1415d122b61/occupancy?&iframeid=occupancyCounter&fId=1038'
 
 def get_occupancy():
     page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
+    content = page.content.decode()
     s = r"'BLD'.* \n\s*'capacity'.*\n\s*'count' : \d{1,3}"
-    re_match = re.search(s, soup.prettify())
+    re_match = re.search(s, content)
     if re_match:
         count_match = re.search(
             r"(?<='count' : )\d{1,3}",
