468adc0917537d4dc4923e3b0d22db4351ea7033
WhiffleFish <tylertronics@gmail.com>
2025-02-01 11:03:25 -0700

    auto csv saving works

--- occupancy_scraper.py
+++ occupancy_scraper.py
@@ -1,6 +1,7 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import datetime
 
 URL = 'https://portal.rockgympro.com/portal/public/415a34a23151c6546419c1415d122b61/occupancy?&iframeid=occupancyCounter&fId=1038'
 
@@ -17,3 +18,11 @@ def get_occupancy():
         return int(count_match.group(0))
     else:
         return float('nan')
+
+def write_to_file(count, file='occupancy.csv'):
+    with open(file, 'a') as file:
+        file.write(', '.join([str(datetime.datetime.now()), str(count)])+'\n')
+
+if __name__ == '__main__':
+    occ = get_occupancy()
+    write_to_file(occ)
