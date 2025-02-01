15d538cf9940a92e15dc25d32fd0a7e04b6bb67b
WhiffleFish <tylertronics@gmail.com>
2025-02-01 10:24:57 -0700

    no need for selenium

--- occupancy_scraper.py
+++ occupancy_scraper.py
@@ -1,26 +1,19 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import TimeoutException
-import time
-import os
+import requests
+import re
+from bs4 import BeautifulSoup
 
-delay = 5 # seconds
+URL = 'https://portal.rockgympro.com/portal/public/415a34a23151c6546419c1415d122b61/occupancy?&iframeid=occupancyCounter&fId=1038'
 
-opts = Options()
-opts.headless = True
-
-browser = Firefox(options=opts, executable_path=os.path.join(os.path.dirname(__file__), 'geckodriver'))
-# browser.get(r'https://thespotgym.com/gym-occupancy')
-browser.get(r'https://portal.rockgympro.com/portal/public/415a34a23151c6546419c1415d122b61/occupancy?&iframeid=occupancyCounter&fId=1038')
-
-try:
-    elem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.ID, 'facility-count')))
-    time.sleep(1.0)
-    print(elem.text)
-except TimeoutException:
-    print("Loading took too much time!")
-
-browser.close()
+def get_occupancy():
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    s = r"'BLD'.* \n\s*'capacity'.*\n\s*'count' : \d{1,3}"
+    re_match = re.search(s, soup.prettify())
+    if re_match:
+        count_match = re.search(
+            r"(?<='count' : )\d{1,3}",
+            re_match.group(0)
+        )
+        return int(count_match.group(0))
+    else:
+        return float('nan')
